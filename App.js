2402ba824ac4a9ac7a8c1ac32db2258f24d6f390
scaceresBS <scaceres@luc.edu>
2020-02-20 00:23:03 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component} from 'react'
-import { StyleSheet, Text, View }  from 'react-native'
+import { StyleSheet, Text, View, ScrollView }  from 'react-native'
 import moment from 'moment' //Used to parse time... Terminal(yarn add moment)
 
 const DATA = {
@@ -7,11 +7,11 @@ const DATA = {
   laps: [12345, 2345, 34567, 98765],
 }
 
-function Timer({ interval}) {
+function Timer({ interval, style}) {
   const duration = moment.duration(interval)
   const centiseconds = Math.floor(duration.milliseconds()/10) //turns milli into 2 dec centi
   return (
-  <Text style={styles.timer}>
+  <Text style={style}>
     {duration.minutes()}:{duration.seconds()}:{centiseconds}
   </Text>
   )
@@ -26,12 +26,60 @@ function RoundButton({ title, color, background}){ //Three proeprties of button
    </View>
  )
 }
+function Lap({ number, interval, fastest, slowest}){
+  const lapStyle = [
+    styles.lapText,
+    fastest && styles.fastest,
+    slowest && styles.slowest,
+  ]
+  return(
+    <View style={styles.lap}>
+      <Text style={lapStyle}>Lap {number}</Text>
+      <Timer style={lapStyle} interval={interval}/>
+    </View>
+  )
+}
+
+function LapsTable({ laps}) {
+  const finishedLaps = laps.slice(1)
+  let min = Number.MAX_SAFE_INTEGER
+  let max = Number.MIN_SAFE_INTEGER
+  if (finishedLaps.length >= 2) {
+    finishedLaps.forEach(lap => {
+      if(lap < min) min = lap
+      if(lap > max) max = lap
+    })
+  }
+  return(
+    <ScrollView style={styles.scrollView}>
+      {laps.map((lap, index) => (
+        <Lap 
+         number={laps.length - index}  //warning? 24:40?
+         key={laps.length - index} 
+         interval={lap} 
+         fastest={lap == min}
+         slowest={lap == max}
+         />
+      ))}
+    </ScrollView>
+  )
+}
+
+function ButtonsRow({ children }) {
+ return(
+   <View style = {styles.buttonsRow}>{ children }</View>
+ )
+}
 export default class App extends Component {
    render() {
      return (
        <View style = {styles.container}>
-         <Timer interval ={DATA.timer}/>
-         <RoundButton title= 'Start' color= '#50D167' background= '#1B361F'/>
+         <Timer interval ={DATA.timer} style={styles.timer}/>
+         <ButtonsRow>
+           <RoundButton title= 'Reset' color= '#FFFFFF' background= '#3D3D3D'/>
+           <RoundButton title= 'Start' color= '#50D167' background= '#1B361F'/>
+         </ButtonsRow>
+         <LapsTable laps={DATA.laps} />
        </View>
      );
    }
@@ -43,6 +91,7 @@ const styles = StyleSheet.create({
      backgroundColor: '#0D0D0D',  //Color of Background 
      alignItems: 'center',
      paddingTop: 130,             //Spaces down text (not hidden from notch)
+     paddingHorizontal: 20,
   },
   timer: {
     color: '#FFFFFF',
@@ -66,8 +115,35 @@ const styles = StyleSheet.create({
     borderWidth:2,
     justifyContent: 'center',
     alignItems: 'center',
-  }
+  },
+  buttonsRow:{
+    flexDirection: 'row',
+    alignSelf: 'stretch',
+    justifyContent: 'space-between',
+    marginTop: 80,
+    marginBottom: 30,
+  },
+  lapText: {
+    color: '#FFFFFF',
+    fontSize: 18,
+  },
+  lap: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderColor: '#151515',
+    borderTopWidth: 1,
+    paddingVertical: 10,
+  },
+  scrollView: {
+    alignSelf: 'stretch',
+  },
+  fastest: {
+    color: '#4BC05F',
+  },
+  slowest: {
+    color: '#CC3531',
+  },
 })
 
-//reference youtube:
-//React Native Tutorial: Build a Complete Stopwatch App in 60 Minutes
+//All athestics are complete... now start working on user input! or actual use!
+//Time: 35:00
